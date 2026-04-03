eb7a5d75b5e9f1145510e2abb320b0a4a52d4ad2
samiyarida-project <samiyaridamd@gmail.com>
2026-04-03 12:03:48 +0530

    Add files via upload

---
+++ Numpy.py
@@ -0,0 +1,20 @@
+
+
+import numpy as np
+
+# 1D array
+arr1 = np.array([10, 20, 30, 40])
+print("1D Array:", arr1)
+
+# 2D array
+arr2 = np.array([[1, 2], [3, 4]])
+print("2D Array:\n", arr2)
+
+# Mean, Min, Max
+mean_val = np.mean(arr1)
+min_val = np.min(arr1)
+max_val = np.max(arr1)
+
+print("Mean:", mean_val)
+print("Minimum:", min_val)
+print("Maximum:", max_val)
