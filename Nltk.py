ce0497368e73b9cdb399f898b14ab7d24f8561c1
samiyarida-project <samiyaridamd@gmail.com>
2026-04-03 23:19:36 +0530

    Add files via upload

---
+++ Nltk.py
@@ -0,0 +1,24 @@
+import nltk
+#nltk.download('punkt')
+#nltk.download('averaged_perceptron_tagger')
+from nltk import word_tokenize, sent_tokenize
+from nltk.stem import PorterStemmer
+from nltk import pos_tag
+
+
+sent ="Tokenization refers to breaking down the text into smaller units." 
+print(word_tokenize(sent))
+print(sent_tokenize(sent))
+
+
+# Create an object of class PorterStemmer
+porter = PorterStemmer()
+print(porter.stem("playing"))
+print(porter.stem("plays"))
+print(porter.stem("played"))
+
+# Part Of Speech Tagging
+text = "It is a Computer Science platform."
+tokenized_text = word_tokenize(text)
+tags = pos_tag(tokenized_text)
+print(tags)
