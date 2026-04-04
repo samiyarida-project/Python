170f9b316f0abc0f308f165b8bf6a8d7cf084420
samiyarida-project <samiyaridamd@gmail.com>
2026-04-04 18:16:51 +0530

    Update Pd_tutorial.py

--- Pd_tutorial.py
+++ Pd_tutorial.py
@@ -15,3 +15,15 @@ print("\n Highest Marks:",df["Marks"].max())
 print("\n Average Marks:",df["Marks"].mean())
 print("\n Lowest Marks:",df["Marks"].min())
 
+output:
+   Name  Marks      College
+0  Harry     85  ABC College
+1   sara     90  ABC College
+2   john     89  ABC College
+
+ Highest Marks: 90
+
+ Average Marks: 88.0
+
+ Lowest Marks: 85
+
