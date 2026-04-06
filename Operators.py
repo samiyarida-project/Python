970dd05bd9fb8073a45e37fa697dad42119d9ed7
samiyarida-project <samiyaridamd@gmail.com>
2026-04-06 23:12:20 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,69 @@
+#Operators
+a,b=56,78
+#Arithmetic Operators(+,-,*,/,%,//,**)
+
+print("Arithmetic:")
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+print("Modulus:",a%b)
+print("Floor Division:",a//b)
+print("Power:",a*b)
+
+#Logical operators(AND,OR,NOT)
+print("\nLogical:")
+print(a>100 and b<90)
+print(a>b or b<50)
+print(not(a>b))
+
+#Comparision Operators(==,!=,>,<,>=,<=)
+print("\nComparision")
+print("Equal:",a==b)
+print("Not Equal:",a!=b)
+print("Greater Than:",a>b)
+print("Less Than:",a<b)
+print("Greater or Equal:",a>=b)
+print("Less than or Equal:",a<=b)
+
+#Assignment Operators(+=,-=,*=,..)
+print("\nAssignment:")      
+a+=5 
+print("a+=:",a)
+b*=17
+print("b*=:",b)
+
+
+
+#Output:
+#Arithmetic:
+#Addition: 134
+#Subtraction: -22
+#Multiplication: 4368
+#Division: 0.717948717948718
+#Modulus: 56
+#Floor Division: 0
+#Power: 4368
+
+#Logical:
+#False
+#False
+#True
+
+#Comparision
+#Equal: False
+#Not Equal: True
+#Greater Than: False
+#Less Than: True
+#Greater or Equal: False
+#Less orEqual: True
+
+#Assignment:
+#a+=: 61
+#b*=: 1326
+
+
+      
+
+
+
