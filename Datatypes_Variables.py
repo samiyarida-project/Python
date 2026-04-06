714b0f6a7474ac508556f9f43dbd07b44be6248c
samiyarida-project <samiyaridamd@gmail.com>
2026-04-06 23:58:51 +0530

    Add files via upload

---
+++ Datatypes_Variables.py
@@ -0,0 +1,74 @@
+#Variables(Memory Location)
+Name="Eleven"
+Age=22
+Place="Hawkins"
+print("Name:",Name)
+print("Age:",Age)
+print("Place:",Place)
+
+#DataTypes
+#Numeric Type
+integer_n = 20
+print("Integer:",integer_n)
+float_f = 5.15
+print("Float:",float_f)
+complex_c = 2 + 7j
+print("Complex:",complex_c)
+
+#String Type
+text_1 = "World"
+text_2 ="Stranger Things"
+print("Hello",text_1)
+print("Welcome",text_2)
+
+#List
+persons=["Michael","Will","Eleven"]
+print("Persons:", persons)
+
+#Tuple
+age=(14,13,12)
+print("Age:", age)
+
+#Set
+roll_no={8,15,11}
+print("Roll Numbers:", roll_no)
+
+#Dictionary (key-value pairs)
+student = {"name": "Harry", "age": 18}
+print("Student:",student)
+
+#Boolean
+Mike_is_leader=True
+print("Is Mike leader?",Mike_is_leader)
+
+#None Type
+will_no=None
+print("Will No:",will_no)
+
+#Output:
+#Name: Eleven
+#Age: 22
+#Place: Hawkins
+#Integer: 20
+#Float: 5.15
+#Complex: (2+7j)
+#Hello World
+#Welcome Stranger Things
+#Persons: ['Michael', 'Will', 'Eleven']
+#Age: (14, 13, 12)
+#Roll Numbers: {8, 11, 15}
+#Student: {'name': 'Harry', 'age': 18}
+#Is Mike leader? True
+#Will No: None
+
+
+
+    
+
+
+
+
+
+
+
+
