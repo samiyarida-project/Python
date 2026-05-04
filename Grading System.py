2db17bdeba5bedd89143f3102be8d5b2a1e6169d
samiyarida-project <samiyaridamd@gmail.com>
2026-05-04 15:25:46 +0530

    Create Grading System.py

---
+++ Grading System.py
@@ -0,0 +1,16 @@
+marks=float(input("Enter your marks:"))
+if marks>=90:
+     print("Grade:A")
+elif marks>=70:
+     print("Grade:B")
+elif marks>=50:
+     print("Grade:C")
+elif marks>=35:
+     print("Grade:D")
+else:
+     print("Fail")
+
+#output:
+#Enter your marks:75
+#Grade:B
+
